cc41fe48a3e9e0310890669614ca9ec5ac0d49f6
Luiz Felipe Silva <luiz.felipe7@gmail.com>
2021-04-06 00:19:19 -0300

    Adds solution to 2020 problem 7, part 2

---
+++ 2020/7-2.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+with open("input7.txt", "r") as f:
+    lines = f.read().splitlines()
+
+def line2colors(s):
+    n = s.count(',')
+    s = s.split()
+    colors = [s[0] + ' ' + s[1]]
+    for i in xrange(4, 4 * n + 5, 4):
+        colors.append((int(s[i]), s[i + 1] + ' ' + s[i + 2]))
+    return colors
+
+g = {}
+for line in lines:
+    if line[-14:-1] == 'no other bags':
+        continue
+    colors = line2colors(line)
+    if colors[0] not in g:
+        g[colors[0]] = []
+    for w, color in colors[1:]:
+        g[colors[0]].append((w, color))
+        if color not in g:
+            g[color] = []
+
+def count(g, color):
+    total = 0
+    for w, neighbor in g[color]:
+        total += w + w * count(g, neighbor)
+    return total
+
+print count(g, 'shiny gold')
